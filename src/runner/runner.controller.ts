db23473252477e9794bc5f4210bbed11719c6330
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-11-23 03:29:22 +0530

    feat: integration with task-runner complete

--- src/runner/runner.controller.ts
+++ src/runner/runner.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ExecuteCodeDto } from './dto/execute-code.dto';
 import { RunnerService } from './runner.service';
 
 @Controller('runner')
 export class RunnerController {
   constructor(private readonly runnerService: RunnerService) {}
+
+  @Post()
+  executeCode(@Body() requestDetails: ExecuteCodeDto) {
+    return this.runnerService.execute(requestDetails);
+  }
 }
--- src/runner/runner.module.ts
+++ src/runner/runner.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { HttpModule, Module } from '@nestjs/common';
 import { RunnerService } from './runner.service';
 import { RunnerController } from './runner.controller';
 
 @Module({
+  imports: [HttpModule],
   controllers: [RunnerController],
-  providers: [RunnerService]
+  providers: [RunnerService],
 })
 export class RunnerModule {}
