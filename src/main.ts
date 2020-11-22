1fdb935bd06abdc8b83605c1183c05a85b2c949b
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-11-22 21:51:30 +0530

    feat: setup configs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as config from 'config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  /** binding port to service */
+  await app.listen(config.get('server.port'));
 }
 bootstrap();
