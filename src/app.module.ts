b0fa2d3ace8f341f85278ace01edd0c8916e1f3f
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-11-22 22:38:34 +0530

    feat: generate resource: questions

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
+import { QuestionsModule } from './questions/questions.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), QuestionsModule],
 })
 export class AppModule {}
