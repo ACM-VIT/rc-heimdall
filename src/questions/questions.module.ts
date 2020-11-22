b390250582397b5208263cab226e27a5fcc198e2
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-11-23 00:51:05 +0530

    feat: entity, service, repository, controller setup

--- src/questions/questions.module.ts
+++ src/questions/questions.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { QuestionsService } from './questions.service';
 import { QuestionsController } from './questions.controller';
+import { QuestionRepository } from './questions.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([QuestionRepository])],
   controllers: [QuestionsController],
-  providers: [QuestionsService]
+  providers: [QuestionsService],
 })
 export class QuestionsModule {}
--- src/questions/questions.repository.ts
+++ src/questions/questions.repository.ts
@@ -1,8 +1,14 @@
 import { EntityRepository, Repository } from 'typeorm';
-import { QuestionEntity } from './entities/question.entity';
+import { GetQuestionDto } from './dto/filter-question.dto';
+import { QuestionEntity } from './question.entity';
 
 @EntityRepository(QuestionEntity)
 export class QuestionRepository extends Repository<QuestionEntity> {
+  /**
+   * Method to clear list of stored Question IDs
+   * @async
+   * @returns boolean true if all questions from persistence later were cleared
+   */
   async clearStorage(): Promise<boolean> {
     try {
       await this.delete(1);
@@ -12,7 +18,13 @@ export class QuestionRepository extends Repository<QuestionEntity> {
     }
   }
 
-  async seedQuestionList(questionList: Array<string>): Promise<Array<{ id }>> {
+  /**
+   * Method to seed question into persistence layer
+   * @async
+   * @param questionList array of strings containing ids of questions downloaded by controller
+   * @returns boolean true if seeding is successful
+   */
+  async seedQuestionList(questionList: Array<string>): Promise<boolean> {
     try {
       const insertionObject = questionList.map(question => {
         return { id: question };
@@ -23,9 +35,23 @@ export class QuestionRepository extends Repository<QuestionEntity> {
         .values(insertionObject)
         .execute();
 
-      return insertionObject;
+      return true;
     } catch (error) {
-      return [];
+      return false;
     }
+  }
+
+  /**
+   * method to check if given question id exists in storage
+   * @async
+   * @param filterDto {id} object containing the ID of the question to check if it exists
+   * @returns boolean true if given question with id exists
+   */
+  async checkIfQuestionExist(filterDto: GetQuestionDto): Promise<boolean> {
+    const { id } = filterDto;
+    const query = this.createQueryBuilder('questions');
+    query.andWhere('questions.id = :id', { id });
+    const result = await query.getMany();
+    return result.length !== 0;
   }
 }
