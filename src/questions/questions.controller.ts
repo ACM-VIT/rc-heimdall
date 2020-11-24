bb5f34f8d41db03ca3f420fba53117928ba62c55
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-11-24 12:20:34 +0530

    feat: plugoff database, rely on memory store

--- src/questions/questions.controller.ts
+++ src/questions/questions.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { QuestionsService } from './questions.service';
 
 @Controller('questions')
--- src/questions/questions.service.ts
+++ src/questions/questions.service.ts
@@ -1,18 +1,28 @@
 import {
+  Dependencies,
+  HttpService,
   Injectable,
   InternalServerErrorException,
   NotFoundException,
+  ServiceUnavailableException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { GetQuestionDto } from './dto/filter-question.dto';
 import { QuestionRepository } from './questions.repository';
+import * as config from 'config';
 
 @Injectable()
+@Dependencies(HttpService)
 export class QuestionsService {
   constructor(
+    private readonly http: HttpService,
+    private readonly endpoint: string,
+
     @InjectRepository(QuestionRepository)
     private questionRepository: QuestionRepository,
-  ) {}
+  ) {
+    this.endpoint = config.get('seeder.endpoint');
+  }
 
   /**
    * Method to seed data from firebase cloud bucket
@@ -21,7 +31,27 @@ export class QuestionsService {
    * @returns Null
    */
   async fetchQuestionsFromFirebase() {
-    return 'This action adds a new question';
+    try {
+      const reply = await this.http.get(this.endpoint).toPromise();
+      if (reply.status !== 200) {
+        throw new ServiceUnavailableException(`Cannot connect to seeder`);
+      }
+      //   clear old question indexes
+      this.questionRepository.clearStorage();
+
+      //   generate list of all questions
+      const questionDetails = reply.data.payload;
+      this.questionRepository.seedQuestionList(
+        questionDetails.map(question => {
+          return question.id;
+        }),
+      );
+
+      return reply.data;
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException('Error seeding');
+    }
   }
 
   /**
@@ -37,21 +67,6 @@ export class QuestionsService {
     );
     if (questionExists !== true) {
       throw new NotFoundException(`No question with id:${filterDto.id} exists`);
-    }
-    return true;
-  }
-
-  /**
-   * method to to clear local storage of programs
-   * @async
-   * @param Null
-   * @returns boolean true if stored questions list is cleared
-   * @throws InternalServerErrorException when operation fails
-   */
-  async clearStoredQuestionList() {
-    const clearTaskStatus = await this.questionRepository.clearStorage();
-    if (clearTaskStatus !== true) {
-      throw new InternalServerErrorException(`Error while clearing data`);
     }
     return true;
   }
