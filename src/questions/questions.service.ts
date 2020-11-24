373dcb55a308777077aea7d634539a063d22326a
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-11-25 05:10:13 +0530

    feat: implement verbose logging

--- src/questions/questions.service.ts
+++ src/questions/questions.service.ts
@@ -3,6 +3,7 @@ import {
   HttpService,
   Injectable,
   InternalServerErrorException,
+  Logger,
   NotFoundException,
   ServiceUnavailableException,
 } from '@nestjs/common';
@@ -18,12 +19,14 @@ export class QuestionsService {
     private readonly http: HttpService,
     private readonly seederEndpoint: string,
     private readonly taskRunnerEndpoint: string,
+    private readonly logger = new Logger('questions'),
 
     @InjectRepository(QuestionRepository)
     private questionRepository: QuestionRepository,
   ) {
     this.seederEndpoint = config.get('seeder.endpoint');
     this.taskRunnerEndpoint = config.get('runner.seedEndpoint');
+    this.logger.verbose('service initiated');
   }
 
   /**
@@ -34,26 +37,34 @@ export class QuestionsService {
    */
   async fetchQuestionsFromFirebase() {
     try {
+      this.logger.verbose(`connecting to seeding endpoint: ${this.seederEndpoint} `);
       const reply = await this.http.get(this.seederEndpoint).toPromise();
+
       if (reply.status !== 200) {
+        this.logger.error(`connecting to seeding endpoint: ${this.seederEndpoint}`);
         throw new ServiceUnavailableException(`Cannot connect to seeder`);
       }
-      //   clear old question indexes
-      this.questionRepository.clearStorage();
 
-      //   generate list of all questions
+      this.logger.verbose('Clearing local question storage');
+      this.questionRepository.clearStorage();
+      this.logger.verbose('Cleared local question storage');
+
+      /** transform object into array */
       const questionDetails = reply.data.payload;
+      this.logger.verbose(`Seeding questions into memory storage: ${questionDetails.length} total`);
       this.questionRepository.seedQuestionList(
         questionDetails.map(question => {
           return question.id;
         }),
       );
+      this.logger.verbose(`Seeding questions complete`);
 
       //   shoot and forget
+      this.logger.verbose('Pinging task-runner to update storages');
       this.pingTaskRunnerToUpdateStorages();
       return reply.data;
     } catch (error) {
-      console.log(error);
+      this.logger.error('seeding questions into memory, throwing 500', error);
       throw new InternalServerErrorException('Error seeding');
     }
   }
@@ -66,12 +77,13 @@ export class QuestionsService {
    * @throws NotFoundException when given question does not exist
    */
   async checkIfQuestionExist(filterDto: GetQuestionDto) {
-    const questionExists = await this.questionRepository.checkIfQuestionExist(
-      filterDto,
-    );
+    this.logger.verbose(`checking if question with id:${filterDto.id} exists`);
+    const questionExists = await this.questionRepository.checkIfQuestionExist(filterDto);
     if (questionExists !== true) {
+      this.logger.verbose(`Question with id:${filterDto.id} does not exist`);
       throw new NotFoundException(`No question with id:${filterDto.id} exists`);
     }
+    this.logger.verbose(`question with id:${filterDto.id} exists`);
     return true;
   }
 
@@ -87,9 +99,11 @@ export class QuestionsService {
    */
   pingTaskRunnerToUpdateStorages() {
     try {
+      this.logger.verbose(`Pinging task-runner on ${this.taskRunnerEndpoint}`);
       this.http.get(this.taskRunnerEndpoint).toPromise();
       return true;
     } catch (e) {
+      this.logger.error(`Pinging task-runner`);
       throw new InternalServerErrorException();
     }
   }
