c5f316c62048b24d3a2a1570e9f064e3f982b100
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-12-01 12:31:10 +0530

    feat: display selected details only to user

--- src/problems/problems.repository.ts
+++ src/problems/problems.repository.ts
@@ -2,4 +2,20 @@ import { EntityRepository, Repository } from 'typeorm';
 import { Problems } from './problem.entity';
 
 @EntityRepository(Problems)
-export class ProblemRepository extends Repository<Problems> {}
+export class ProblemRepository extends Repository<Problems> {
+  async findAndFilter(id: string): Promise<Problems> {
+    const query = this.createQueryBuilder()
+      .select([
+        'problems.id',
+        'problems.maxPoints',
+        'problems.windowsFileURL',
+        'problems.objectFileURL',
+        'problems.instructionsText',
+      ])
+      .from(Problems, 'problems')
+      .andWhere('problems.id = :id', { id })
+      .getOne();
+
+    return query;
+  }
+}
