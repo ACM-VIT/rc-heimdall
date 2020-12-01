7186bd7f4bdb887da720e9e08255f548adea5bc0
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-12-01 12:34:25 +0530

    feat: Delete questions module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
-import { QuestionsController } from './questions/questions.controller';
-import { QuestionsModule } from './questions/questions.module';
 import { RunnerController } from './runner/runner.controller';
 import { RunnerModule } from './runner/runner.module';
 import { TeamsModule } from './teams/teams.module';
@@ -15,7 +13,6 @@ import { SyncModule } from './sync/sync.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
-    QuestionsModule,
     RunnerModule,
     TeamsModule,
     ParticipantsModule,
@@ -28,6 +25,6 @@ import { SyncModule } from './sync/sync.module';
 // JudgeModule,
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes(QuestionsController, RunnerController);
+    consumer.apply(LoggerMiddleware).forRoutes(RunnerController);
   }
 }
--- src/questions/dto/filter-question.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class GetQuestionDto {
-  @ApiProperty({
-    description: 'id of the question to check if it exists',
-    required: true,
-  })
-  id: string;
-}
