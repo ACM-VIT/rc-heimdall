c31b764ce2fe81ceed4bfe247d0510208bc662f9
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-12-02 13:36:39 +0530

    feat: standardize  output by mapping into array

--- src/judge/judge.controller.ts
+++ src/judge/judge.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get, Post, Body, Put, Param, Delete, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Put, Param, Delete, UsePipes, ValidationPipe, Logger } from '@nestjs/common';
 import { JudgeService } from './judge.service';
 import { CreateJudgeDto } from './dto/create-judge.dto';
 import { UpdateJudgeDto } from './dto/update-judge.dto';
 import { CallbackJudgeDto } from './dto/callback-judge.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { CODE_STATES, DILUTE } from './enum/codeStates.enum';
 
 @ApiTags('Judge')
 @Controller('judge')
 export class JudgeController {
+  private readonly logger = new Logger('judge controller');
   constructor(private readonly judgeService: JudgeService) {}
 
   @Post()
@@ -29,7 +31,7 @@ export class JudgeController {
   @Put('callback')
   @UsePipes(ValidationPipe)
   callbackHandler(@Body() callbackJudgeDto: CallbackJudgeDto) {
-    console.log('checking');
+    this.logger.verbose(`Received callback for ${callbackJudgeDto.token} :: ${DILUTE[callbackJudgeDto.status.id]}`);
     return this.judgeService.handleCallback(callbackJudgeDto);
   }
 
