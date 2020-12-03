247d4502c39d45a6833a894684ee87bad313a3ca
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-12-03 23:22:10 +0530

    docs: add docs for task-runner

--- src/runner/runner.service.ts
+++ src/runner/runner.service.ts
@@ -1,14 +1,30 @@
-import { Dependencies, HttpService, Injectable, Logger, NotFoundException } from '@nestjs/common';
-import { ExecuteCodeDto } from './dto/execute-code.dto';
 import * as config from 'config';
 
+import { Dependencies, HttpService, Injectable, Logger, NotFoundException } from '@nestjs/common';
+
+import { ExecuteCodeDto } from './dto/execute-code.dto';
+
+/**
+ * **Runner Service**
+ *
+ * Runner Service contains all business logic related to code-runner, and is designed to be
+ * imported and re-used in other modules. Therefore it is to ensure that all methods of the service
+ * are self-contained and fit to be used directly as per use-case.
+ *
+ * @category Runner
+ */
 @Injectable()
 @Dependencies(HttpService)
 export class RunnerService {
   /** import http service to make web requests to task-runner */
   constructor(
+    /** [[HttpService]] to make HTTP calls to task runner */
     private readonly http: HttpService,
+
+    /** endpoint to make http calls at */
     private readonly endpoint: string,
+
+    /** initiate logger with context:`runner` */
     private readonly logger = new Logger('runner'),
   ) {
     this.endpoint = config.get('runner.runEndpoint');
@@ -20,11 +36,10 @@ export class RunnerService {
    * Method to fetch code output from task-runner by making a POST request to the
    * task-runner /run endpoint.
    *
-   * @async
-   * @param executeCode Object containing {id, input} of program to run
-   * @returns Promise<string> that contains output of code
-   * @throws InternalServerErrorException(500) when faces error while processing request
-   * @throws ServiceUnavailableException(503) when faces too many requests at same time
+   * **Response**
+   * - InternalServerErrorException(500) when faces error while processing request
+   * - ServiceUnavailableException(503) when faces too many requests at same time
+   *
    * @see TaskRunner for more details about code execution service. https://github.com/YashKumarVerma/rc-task-runner
    */
   async execute(executeCode: ExecuteCodeDto): Promise<string> {
